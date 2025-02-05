b37bc7e7640b1c30afe843fab6575c1a08e59321
bscit-05-39008695 <aronmutai99@gmail.com>
2025-02-05 20:15:16 +0300

    updated

--- server/app.py
+++ server/app.py
@@ -1,26 +1,79 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response
 from flask_cors import CORS
 from flask_migrate import Migrate
-
 from models import db, Message
+from flask import jsonify
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
 CORS(app)
-migrate = Migrate(app, db)
 
 db.init_app(app)
+migrate = Migrate(app, db)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return make_response(
+        jsonify([message.to_dict() for message in messages]),  # Add jsonify
+        200
+    )
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    
+    new_message = Message(
+        body=data.get('body'),
+        username=data.get('username')
+    )
+    
+    db.session.add(new_message)
+    db.session.commit()
+    
+    return make_response(
+        new_message.to_dict(),
+        200
+    )
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.filter_by(id=id).first()
+    
+    if not message:
+        return make_response(
+            {'error': 'Message not found'},
+            400
+        )
+    
+    data = request.get_json()
+    for attr in data:
+        setattr(message, attr, data[attr])
+    
+    db.session.commit()
+    
+    return make_response(
+        message.to_dict(),
+        200
+    )
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.filter_by(id=id).first()
+    
+    if not message:
+        return make_response(
+            {'error': 'Message not found'},
+            400
+        )
+    
+    db.session.delete(message)
+    db.session.commit()
+    
+    return make_response('', 200)
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
